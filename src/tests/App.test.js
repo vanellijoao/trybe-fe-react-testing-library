b8c67ca566351c005eab098cd2b880a39a67993c
Joao Vanelli <vanellijoao@gmail.com>
2021-05-26 16:56:24 -0300

    Adicionando teste da Navbar

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testing the <App /> component', () => {
+  it('a heading with the text \'Pokédex\' is rendered', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const pokemonHeading = getByText(/pokédex/i);
+
+    expect(pokemonHeading).toBeInTheDocument();
+  });
+
+  it('the navbar is rendered with \'Home\', \'About\' and \'Favorite Pokémons\'', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/home/i);
+    const about = getByText(/about/i);
+    const favoritePokemons = getByText('Favorite Pokémons');
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
 });
