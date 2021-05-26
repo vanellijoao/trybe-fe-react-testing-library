3b9776dc8d8098f44ad065b57d37fb0f19bb43e8
Joao Vanelli <vanellijoao@gmail.com>
2021-05-26 17:51:27 -0300

    Testes componente <Favorite Pokemons />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithRouter from '../services/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testing the <FavoritePokemons /> component', () => {
+  it('', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noPokes = getByText('No favorite pokemon found');
+
+    expect(noPokes).toBeInTheDocument();
+  });
+});
