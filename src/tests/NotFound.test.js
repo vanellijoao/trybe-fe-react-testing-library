0ef5d333be0500657d6f1cf6af59f0e9d4debbe9
Joao Vanelli <vanellijoao@gmail.com>
2021-05-26 18:00:01 -0300

    Testes componente <NotFound />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testing the <NotFound /> component', () => {
+  it('', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const notFound = getByText('Page requested not found');
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('an img on it', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+    const img = getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
